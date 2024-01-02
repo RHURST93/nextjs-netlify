f12cea22a45c1c9318ea8a5d1c1d4136f524cffb
Randy Hurst <rahurst93@gmail.com>
2024-01-01 22:48:47 -0600

    revised css

--- next.config.js
+++ next.config.js
@@ -7,16 +7,3 @@ module.exports = nextConfig
 
 const path = require('path');
 
-module.exports = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(pdf)$/,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/files/[name][ext]',
-      },
-    });
-
-    return config;
-  },
-};
