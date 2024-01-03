87fe945d6ac4eadbb68b2271c1169a3d44071e1a
Randy Hurst <rahurst93@gmail.com>
2024-01-03 11:58:25 -0600

    footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,34 +7,38 @@ import { CButton } from '@coreui/react';
 const Footer = () => {
   return (
     <footer className="bg-base-300 text-white py-4">
-      <div className=" grid-cols-1 container mx-auto  flex-col md:flex-row ">
-        <div className="md:order-2 grid grid-cols-11">
-          <Image className='ml-5' src={code} alt='logo' />
+      <div className="container mx-auto md:flex md:justify-between">
+        {/* Left Column */}
+        <div className="md:w-1/2 md:order-1 flex items-center justify-center md:justify-start">
+          <Image className="ml-5" src={code} alt="logo" width={60} height={60} />
         </div>
-        <div className=''>
-             <NextLink className='no-underline grid  ' href="mailto:rahurst93@gmail.com">
-            
+
+        {/* Right Column */}
+        <div className="md:w-1/2 md:order-3 flex flex-col items-center justify-center md:justify-end space-y-4 md:space-y-0 md:flex-row md:space-x-4 md:text-left">
+          {/* Links */}
+          <div className="flex flex-col items-center md:items-start">
+            <h3 className="text-slate-600 underline">Links</h3>
+            <NextLink href="/" className="no-underline">
+              Home
+            </NextLink>
+            <NextLink href="#about" className="no-underline">
+              About
+            </NextLink>
+            <NextLink href="#projects" className="no-underline">
+              Projects
+            </NextLink>
+          </div>
+
+          {/* Contact Me Button */}
+          <NextLink href="mailto:rahurst93@gmail.com" className="no-underline">
             <CButton color="primary">Contact Me</CButton>
-          
-        </NextLink>
+          </NextLink>
         </div>
-        <div className="grid grid-cols-4 space-y-0 md:space-x-2 md:space-y-0 md:flex-1 no-underline">
-            <div className='col-span-1'>
-            <h3 className='text-slate-600 underline justify-center' >Links</h3>
-          <NextLink className='no-underline' href="/">Home
-          </NextLink>
-          <NextLink className='no-underline' href="#about">About
-          </NextLink>
-          <NextLink className='no-underline' href="#projects">Projects
-          </NextLink>
-            </div>
-        </div>
-        
-        <div className="text-center mt-4 md:mt-0 text-gray-400 md:order-3">
-        
+
+        {/* Bottom Text */}
+        <div className="text-center mt-4 md:mt-0 text-gray-400 md:order-2">
           &copy; {new Date().getFullYear()} Randy Hurst. All Rights Reserved.
         </div>
-        
       </div>
     </footer>
   );
